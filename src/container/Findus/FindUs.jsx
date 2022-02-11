5b6aaba8bb1d2f863757d6ed6100dc6f03337330
RoughGhost <Stephendlouis6@gmail.com>
2022-02-11 15:39:11 +0100

    final commit

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: "3rem" }}>
+        Find Us
+      </h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">No 1 Ave Marie Street, Roddithun Avenue.</p>
+        <p
+          className="p__cormorant"
+          style={{ color: "#dcca87", margin: "2rem 0" }}
+        >
+          Opening Hours
+        </p>
+        <p className="p__opensans">Mon - Fri: 10:00am - 17:00pm</p>
+        <p className="p__opensans">Sat - Sun :2:00pm - 22:00pm</p>
+      </div>
+      <button className="custom__button" style={{ marginTop: "2rem" }}>
+        Visit Us
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
-
-import './Footer.css';
+import React from "react";
+import { FooterOverlay, Newsletter } from "../../components";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { images } from "../../constants";
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">Ave Marie Street, Road 24, Hurdland.</p>
+        <p className="p__opensans">+1-909-909-2339</p>
+        <p className="p__opensans">+1-909-909-2009</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">
+          " The Best Way to Find Yourself is to lose Yourself to the service of
+          Others."
+        </p>
+        <img
+          src={images.spoon}
+          alt="spoon"
+          className="spoon__img"
+          style={{ marginTop: 15 }}
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Firday:</p>
+        <p className="p__opensans">08:00am - 12:00am</p>
+        <p className="p__opensans">Sat - Sun:</p>
+        <p className="p__opensans">07:00am - 10:00pm</p>
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2022 Gericht. All Rights Reserved.</p>
+    </div>
   </div>
 );
 
